c86ca3d90159e17a9504e92685b4cd7d2306004c
Ice <wangxiangbing@zhiyihealth.com.cn>
2020-01-06 10:30:49 +0800

    complete linked node merge

--- IcePrivateCocoaPods/Classes/LinkedNode.swift
+++ IcePrivateCocoaPods/Classes/LinkedNode.swift
@@ -18,7 +18,26 @@ open class LinkedNode {
 }
 
 open class LinkedNodeGenerator {
-    func merge(node1: LinkedNode, node2: LinkedNode) -> LinkedNode {
-        return node1
+    public func merge(node1: LinkedNode, node2: LinkedNode) -> LinkedNode {
+        let isHead = node1.value <= node2.value
+        var head = isHead ? node1 : node2
+        var tempNode = head
+        var tail = isHead ? node2 : node1
+        var midNode = isHead ? node1.next : node2.next
+        while tempNode != nil  && tail != nil {
+            if midNode == nil {
+                tempNode.next = tail
+                break
+            } else if midNode.value <= tail.value {
+                tempNode.next = midNode
+                tempNode = midNode
+                midNode = midNode.next
+            } else {
+                tempNode.next = tail
+                tempNode = tail
+                tail = tail.next
+            }
+        }
+        return head
     }
 }
