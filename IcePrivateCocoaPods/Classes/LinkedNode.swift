7a00b266dc50de5b9096580402265d702681386d
Ice <wangxiangbing@zhiyihealth.com.cn>
2020-01-06 10:46:12 +0800

    correct swift programmer

--- IcePrivateCocoaPods/Classes/LinkedNode.swift
+++ IcePrivateCocoaPods/Classes/LinkedNode.swift
@@ -9,7 +9,7 @@ import Foundation
 
 open class LinkedNode {
     var value: Int
-    var next: LinkedNode?
+    public var next: LinkedNode?
     
     public init(value: Int, next: LinkedNode?) {
         self.value = value
@@ -20,22 +20,22 @@ open class LinkedNode {
 open class LinkedNodeGenerator {
     public func merge(node1: LinkedNode, node2: LinkedNode) -> LinkedNode {
         let isHead = node1.value <= node2.value
-        var head = isHead ? node1 : node2
-        var tempNode = head
-        var tail = isHead ? node2 : node1
-        var midNode = isHead ? node1.next : node2.next
+        let head = isHead ? node1 : node2
+        var tempNode: LinkedNode? = head
+        var tail: LinkedNode? = isHead ? node2 : node1
+        var midNode: LinkedNode? = isHead ? node1.next : node2.next
         while tempNode != nil  && tail != nil {
             if midNode == nil {
-                tempNode.next = tail
+                tempNode!.next = tail
                 break
-            } else if midNode.value <= tail.value {
-                tempNode.next = midNode
+            } else if midNode!.value <= tail!.value {
+                tempNode!.next = midNode
                 tempNode = midNode
-                midNode = midNode.next
+                midNode = midNode!.next
             } else {
-                tempNode.next = tail
+                tempNode!.next = tail
                 tempNode = tail
-                tail = tail.next
+                tail = tail!.next
             }
         }
         return head
